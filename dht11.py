af727e8222617af4a9c19af365c34bcefdeb7bc9
Enzo Avagliano <eavagliano6@gmail.com>
2026-03-08 15:40:05 +0100

    Added dht read

---
+++ dht11.py
@@ -0,0 +1,14 @@
+import board
+import adafruit_dht
+from time import sleep
+
+dht_sensor = adafruit_dht.DHT11(board.D4)
+
+while True :
+  try: 
+    temp = dht_sensor.temperature
+    humidity = dht_sensor.humidity
+    print("Temperature: {}°C".format(temp))
+    print("Humidity: {}%".format(humidity))
+  except RuntimeError as e :
+    print("Error: {}".format(e))
