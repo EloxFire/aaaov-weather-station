4bd0d9851505a64c1cb80d91e40ecc868438189f
Enzo Avagliano <eavagliano6@gmail.com>
2026-03-08 16:18:35 +0100

    Updated main file

--- main.py
+++ main.py
@@ -6,14 +6,21 @@
 # Contact AAAOV : contact@aaaov.fr
 # =========
 
+from time import sleep
+
 import dht11
+import bme280
 
 
 def main():
     dht11.main()
+    bme280.main()
 
-    print("Temperature: {}°C".format(dht11.temp))
-    print("Humidity: {}%".format(dht11.humidity))
+    while True:
+        print("Temperature: {}°C".format(dht11.temp))
+        print("Humidity: {}%".format(dht11.humidity))
+        print("BME280 Temperature: {}°C".format(bme280.T))
+        sleep(2)
 
 if __name__ == "__main__":
     print("=== STATION MÉTÉO - AAAOV ===")
